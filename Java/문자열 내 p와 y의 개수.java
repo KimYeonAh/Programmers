402a37cd2e40151668e31e561448aa3e92bb4788
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2021-08-30 18:03:10 +0900

    Create 문자열 내 p와 y의 개수.java

---
+++ Java/문자열 내 p와 y의 개수.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int countP = 0;
+        int countY = 0;
+        
+        for(int i = 0 ; i < s.length(); i ++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P'){
+                countP ++;
+            }
+            
+            if(s.charAt(i) == 'y' || s.charAt(i) == 'Y'){
+                countY ++;
+            }
+        }
+        
+        if(countP != countY){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
